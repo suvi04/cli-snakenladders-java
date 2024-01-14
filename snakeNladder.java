f1dd9ef45962aca48512f50c4cd6dcefa5742ce1
Suvansh <113202968+suvi04@users.noreply.github.com>
2024-01-14 22:10:07 +0530

    Update snakeNladder.java

--- snakeNladder.java
+++ snakeNladder.java
@@ -8,6 +8,79 @@ public class snakeNladder {
 		int res = rand.nextInt(6)+1;
 		return res;
 	}
+	
+	static int stair_snakes(int pos)
+	{
+		switch(pos) 
+		{
+		case 1:
+			pos=38;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 4:
+			pos=14;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 8:
+			pos=30;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 21:
+			pos=42;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 28:
+			pos=74;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 50:
+			pos=67;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 71:
+			pos=92;
+			System.out.println("Climbed a ladder !");
+			break;
+		case 80:
+			pos=99;
+			System.out.println("Climbed a ladder !");
+			break;
+			
+			
+		case 32:
+			pos=10;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 36:
+			pos=6;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 62:
+			pos=18;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 48:
+			pos=26;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 88:
+			pos=24;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 95:
+			pos=56;
+			System.out.println("Got bit by a Snake !");
+			break;
+		case 97:
+			pos=7;
+			System.out.println("Got bit by a Snake !");
+			break;
+		default:
+			pos=pos;
+		}
+		return pos;
+	}
+	
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -34,13 +107,15 @@ public class snakeNladder {
 			int inc = dice();
 			System.out.println(p1_name+" rolled a "+inc);
 			p1_posn+=inc;
+			p1_posn=stair_snakes(p1_posn);
+			
 			if(p1_posn>100)
 				{
 				System.out.println("Position greater than 100, move cancelled!");
 				p1_posn-=inc;
 				System.out.println(p1_name+" stays at "+p1_posn);
 				}
-			if(p1_posn==100)
+			else if(p1_posn==100)
 			{
 				System.out.println(p1_name+" Won!!");
 			}
@@ -54,6 +129,8 @@ public class snakeNladder {
 			int inc2=dice();
 			System.out.println(p2_name+" rolled a "+inc2);
 			p2_posn+=inc2;
+			p1_posn=stair_snakes(p1_posn);
+			
 			if(p2_posn>100)
 				{
 				System.out.println("Position greater than 100, move cancelled!");
